4f5c5ad2ddffc38b86cf91497d02fbf84a01151c
AdlerCastro <adlercastro01@gmail.com>
2024-08-28 18:33:52 -0300

    :sparkles: feat: Schema da nova unidade consumidora

---
+++ src/schemas/new-consumer-unit.schema.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+export const NewConsumerUnitSchema = z.object({
+    number: z
+        .string({
+            required_error: "Campo obrigatório",
+        })
+        .min(
+            8, "O número da unidade consumidora é obrigatório e deve conter no mínimo 8 dígitos"
+        )
+        .max(8, "O número da unidade consumidora deve conter no mínimo 8 dígitos"),
+    description: z
+        .string()
+        .max(255, "A descrição deve ter no máximo 255 caracteres"),
+    city: z
+        .string({
+            required_error: "Campo obrigatório",
+        })
+        .max(255, "A cidade deve ter no máximo 255 caracteres"),
+    uf: z
+        .string({
+            required_error: "Campo obrigatório",
+        })
+        .min(2, "UF deve ter no mínimo 2 caracteres")
+        .min(2, "UF deve ter no máximo 2 caracteres"),
+    cod_concessionaire: z.enum([""], {
+        message: "Concessionária inválida",
+    }),
+    subGroup: z
+        .string({
+          required_error: "Campo obrigatório",
+        })
+        .min(1)
+        .max(10, "O subgrupo deve ter no máximo 10 caracteres"),
+    optanteTB: z.boolean({
+        required_error: "Campo obrigatório"
+    })
+});
+
+export type NewConsumerUnitSchemaType = z.infer<typeof NewConsumerUnitSchema>;
