385a9fa1e883ef65c729c9842d4998c07008b727
AdlerCastro <adlercastro01@gmail.com>
2024-08-28 22:18:56 -0300

    :sparkles: feat: Adicionando inputs e selects para o cadastro

--- src/app/(with-auth)/consumer-unit/new/page.tsx
+++ src/app/(with-auth)/consumer-unit/new/page.tsx
@@ -1,5 +1,23 @@
 "use client";
 import { newConsumerUnitAction } from "@/action/new-consumer-unit-action.action";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import Input from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/components/ui/use-toast";
 import { Routes } from "@/enums/Routes.enum";
 import useCities from "@/hooks/useCities";
@@ -62,5 +80,173 @@ export default function NewConsumerUnitPage() {
     }
   }
 
-  return <h1>Cadastrar unidade</h1>;
+  return (
+    <div className="flex flex-col-reverse items-center lg:grid lg:grid-cols-2 lg:p-14 py-6 gap-9">
+      <div className="flex flex-col items-center lg:items-start w-full space-y-6 col-span-1">
+        <h1 className="hidden lg:block text-3xl font-bold text-secondary-foreground">
+          <span className="text-solaris-primary">Cadastrar</span> nova unidade
+          consumidora
+        </h1>
+        <Form {...form}>
+          <form
+            className="w-full max-w-[500px]"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="space-y-6">
+              <FormField
+                control={form.control}
+                name="number"
+                render={({ field }) => (
+                  <FormItem className="max-w-96">
+                    <FormControl>
+                      <Input
+                        required
+                        {...field}
+                        label="Número da unidade consumidora"
+                        placeholder="Número..."
+                        invalid={!!form.formState.errors.number}
+                        disabled={loading}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex flex-row flex-wrap w-full gap-3 sm:gap-5">
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem className="max-w-40 w-full">
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          disabled={uf === null || citiesLoading || loading}
+                        >
+                          <FormControl>
+                            <SelectTrigger
+                              label="Cidade"
+                              required
+                              invalid={!!form.formState.errors.city}
+                            >
+                              <SelectValue placeholder="Belém" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {cities &&
+                              cities.map((city) => (
+                                <SelectItem key={city.nome} value={city.nome}>
+                                  {city.nome}
+                                </SelectItem>
+                              ))}
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="uf"
+                  render={({ field }) => (
+                    <FormItem className="max-w-40 w-full">
+                      <FormControl>
+                        <Select
+                          onValueChange={(value) => {
+                            field.onChange(value);
+                            setUf(
+                              ufs?.find((uf) => uf.sigla === value)?.id ?? null
+                            );
+                          }}
+                          defaultValue={field.value}
+                          disabled={ufLoading || loading}
+                        >
+                          <FormControl>
+                            <SelectTrigger
+                              label="UF"
+                              required
+                              invalid={!!form.formState.errors.uf}
+                            >
+                              <SelectValue
+                                placeholder={ufLoading ? "Carregando..." : "PA"}
+                              />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {ufs &&
+                              ufs.map((uf) => (
+                                <SelectItem key={uf.sigla} value={uf.sigla}>
+                                  {uf.sigla}
+                                </SelectItem>
+                              ))}
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex flex-row flex-wrap gap-3 w-full sm:gap-5">
+                <FormField
+                  control={form.control}
+                  name="subGroup"
+                  render={({ field }) => (
+                    <FormItem className="max-w-24 w-full">
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          disabled={loading}
+                        >
+                          <FormControl>
+                            <SelectTrigger
+                              label="Subgrupo"
+                              required
+                              invalid={!!form.formState.errors.subGroup}
+                            >
+                              <SelectValue placeholder="A1" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="A1">A1</SelectItem>
+                            <SelectItem value="A2">A2</SelectItem>
+                            <SelectItem value="A3">A3</SelectItem>
+                            <SelectItem value="A4">A4</SelectItem>
+                            <SelectItem value="A3a">A3a</SelectItem>
+                            <SelectItem value="AS">AS</SelectItem>
+                            <SelectSeparator />
+                            <SelectItem value="B1">B1</SelectItem>
+                            <SelectItem value="B2">B2</SelectItem>
+                            <SelectItem value="B3">B3</SelectItem>
+                            <SelectItem value="B4">B4</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <Button
+              type="submit"
+              variant="solar"
+              className="w-full max-w-[500px] mt-12"
+              disabled={loading}
+              loading={loading}
+            >
+              Finalizar Cadastro
+            </Button>
+          </form>
+        </Form>
+      </div>
+      <h1 className="lg:hidden text-2xl sm:text-3xl font-bold text-secondary-foreground">
+        <span className="text-solaris-primary">Cadastrar</span> nova unidade
+        consumidora
+      </h1>
+    </div>
+  );
 }
