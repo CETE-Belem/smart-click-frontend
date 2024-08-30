795366920b1795a37700fca09512c783d24b41ee
AdlerCastro <adlercastro01@gmail.com>
2024-08-30 18:26:45 -0300

    :sparkles: feat: Função de editar unidade consumidora

---
+++ src/action/edit-consumer-unit.action.ts
@@ -0,0 +1,58 @@
+'use server'
+
+import { api } from "@/lib/axios"
+import { NewConsumerUnitSchema, NewConsumerUnitSchemaType } from "@/schemas/new-consumer-unit.schema"
+import { cookies } from "next/headers"
+import { NewConsumerUnitDataType } from "./new-consumer-unit-action.action"
+
+export async function adminEditConsumerUnitAction(
+    formData: NewConsumerUnitSchemaType,
+    cod_consumer_unit: string
+): Promise<{success: boolean; message: string}> {
+    try {
+        const result = NewConsumerUnitSchema.safeParse(formData);
+        const newFormData = result.data!;
+        const token = cookies().get("token")?.value
+        const parsedData: NewConsumerUnitDataType = {
+            numero: newFormData.number,
+            subgrupo: newFormData.subGroup,
+            cidade: newFormData.city,
+            uf: newFormData.uf,
+            optanteTB: newFormData.optanteTB,
+            cod_concessionaria: newFormData.cod_concessionaire,
+        };
+
+        const response = await api
+        .put(`/consumer-unit/${cod_consumer_unit}`, parsedData, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        .then((response) => response)
+        .catch((error) => error.response.data)
+        console.log(response)
+        if(response.status === 200) {
+            return {
+                success: true,
+                message: "Unidade consumidora editado com sucesso"
+            }
+        }
+
+        if (response.statusCode === 404) {
+            return {
+              success: false,
+              message: "Unidade consumidora não encontrado",
+            };
+          }
+
+        return {
+            success: false,
+            message: response.message,
+          };
+    } catch (error) {
+        return {
+            success: false,
+            message: "Erro ao editar equipamento. Exceção: " + error,
+        };
+    }
+}
