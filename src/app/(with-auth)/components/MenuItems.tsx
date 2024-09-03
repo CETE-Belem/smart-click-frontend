bbc0ca7ded408d956f9a82193d3bf145a5130778
AdlerCastro <adlercastro01@gmail.com>
2024-09-03 15:46:18 -0300

    :bug: fix: Link das unidades consumidoras

--- src/app/(with-auth)/components/MenuItems.tsx
+++ src/app/(with-auth)/components/MenuItems.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Briefcase, Building, Zap } from "lucide-react";
 import SidebarMenuItem from "./SidebarMenuItem";
 import { Routes } from "@/enums/Routes.enum";
+// Importação sem uso
 import { Role } from "@/enums/Role.enum";
 import useUserStore from "@/store/user.store";
 
 export default function MenuItems() {
+  // Variável sem uso
   const user = useUserStore((state) => state.user);
   return (
     <>
-      <SidebarMenuItem href={Routes.ConsumerUnit} label="Unidades Consumidoras">
+      <SidebarMenuItem
+        href={Routes.ConsumerUnits}
+        label="Unidades Consumidoras"
+      >
         <Building height={24} width={24} />
       </SidebarMenuItem>
       <SidebarMenuItem href={Routes.Equipments} label="Equipamentos">
