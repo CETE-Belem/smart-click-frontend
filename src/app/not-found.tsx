1b99b0cabd127c44a8c0e938015e3e73f3ae9049
AdlerCastro <adlercastro01@gmail.com>
2024-09-06 13:09:35 -0300

    :sparkles: feat: Botão de retorno a página anterior

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import notFoundImage from "public/images/404-error.svg";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <div className="w-screen h-screen flex items-center justify-center text-">
       <div className="w-full h-full px-7 flex flex-col gap-5 items-center justify-center max-w-[630px] max-h-[710px]">
@@ -14,8 +18,8 @@ export default function NotFound() {
           Desculpe, não conseguimos encontrar o que você procura. Volte para a
           tela principal e tente novamente.
         </p>
-        <Button variant="solar">
-          <Link href="/">Voltar a página principal</Link>
+        <Button variant="solar" onClick={router.back}>
+          Voltar a página principal
         </Button>
         <Image
           src={notFoundImage}
