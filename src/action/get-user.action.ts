108c7aa8e6c3f39da921054a93de7d9e8bdc1233
AdlerCastro <adlercastro01@gmail.com>
2024-09-07 13:23:30 -0300

    :sparkles: feat: Função de resgate de um único usuário

---
+++ src/action/get-user.action.ts
@@ -0,0 +1,22 @@
+'use server'
+
+import { api } from "@/lib/axios"
+import { cookies } from "next/headers"
+import { IUser } from "@/types/IUser"
+
+export interface GetUserResponse {
+    user: IUser
+}
+
+export async function GetUserAction(id: string): Promise<GetUserResponse | any> {
+    const token = cookies().get("token")?.value
+    const data = await api.get<GetUserResponse>(
+        `/concessionaires/${id}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+    return data
+}
