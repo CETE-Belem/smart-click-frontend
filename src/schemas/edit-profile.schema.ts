00c97826afb583c94ebaf9e6fa4628c995e5a723
AdlerCastro <adlercastro01@gmail.com>
2024-09-10 18:02:35 -0300

    :sparkles: feat: Schema de edição do perfil logado

---
+++ src/schemas/edit-profile.schema.ts
@@ -0,0 +1,27 @@
+import z from "zod"
+import { maxEmailLength, maxNameLength, maxPasswordLength, minPasswordLength, passwordRegex } from "@/constants/auth"
+
+export const editProfileSchema = z.object({
+    email: z
+        .string().email("E-mail inválido")
+        .min(1, "O e-mail é obrigatório")
+        .max(maxEmailLength, `O e-mail deve ter no máximo ${maxEmailLength} caracteres`),
+    password: z
+        .string().regex(passwordRegex, "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial")
+        .min(minPasswordLength, `A senha deve ter no mínimo ${minPasswordLength} caracteres`)
+        .max(maxPasswordLength, `A senha deve ter no máximo ${maxPasswordLength} caracteres`),
+    confirmPassword: z
+        .string().regex(passwordRegex, "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial")
+        .min(minPasswordLength, `A senha deve ter no mínimo ${minPasswordLength} caracteres`)
+        .max(maxPasswordLength, `A senha deve ter no máximo ${maxPasswordLength} caracteres`),
+    name: z
+        .string().min(1, "O nome é obrigatório")
+        .max(maxNameLength, `O nome deve ter no máximo ${maxNameLength} caracteres`)
+    })
+    .refine(data => data.password === data.confirmPassword, {
+        message: "As senhas não coincidem",
+    })
+
+export type editProfileSchemaType = z.infer<typeof editProfileSchema>
+
+export const editProfileSchemaTransformed = editProfileSchema.transform(({ confirmPassword, ...data }) => data);
