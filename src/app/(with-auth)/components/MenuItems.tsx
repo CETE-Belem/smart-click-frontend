a12b78c24beb8395c0cedd3f9ed0734dda3b3877
Adler Castro <adlercastro01@gmail.com>
2024-09-17 19:04:19 -0300

    :bug: fix: Duplicação de rotas

--- src/app/(with-auth)/components/MenuItems.tsx
+++ src/app/(with-auth)/components/MenuItems.tsx
@@ -11,20 +11,13 @@ export default function MenuItems() {
   const user = useUserStore((state) => state.user);
   return (
     <>
-      {user?.perfil === Role.ADMIN && (
-        <SidebarMenuItem href={Routes.Users} label="Usuários">
-          <UserRound size={24} />
-        </SidebarMenuItem>
-      )}
-      <SidebarMenuItem href={Routes.Concessionaires} label="Concessionárias">
-        <Zap height={24} width={24} />
-      </SidebarMenuItem>
       <SidebarMenuItem
         href={Routes.ConsumerUnits}
         label="Unidades Consumidoras"
       >
         <Building height={24} width={24} />
       </SidebarMenuItem>
+
       <SidebarMenuItem href={Routes.Equipments} label="Equipamentos">
         <Briefcase height={24} width={24} />
       </SidebarMenuItem>
