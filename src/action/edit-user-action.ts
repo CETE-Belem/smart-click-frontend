4cb046361b348057a6496c4555f50fcab26c55a3
Marco Antônio <marcopierozan20@gmail.com>
2024-09-18 15:52:13 -0300

    ✨ (edit-user-action.ts): melhora a estrutura do código e adiciona tratamento de erros ✨ (get-user.action.ts): refatora a função GetUserAction para melhor legibilidade ♻️ (EditProfileForm.tsx): refatora o componente para usar o estado do usuário e melhora a experiência do usuário
    ♻️ (edit-profile.schema.ts): refatora o esquema de edição de perfil para melhorar a legibilidade e a validação das mensagens de erro.
    
    As mudanças foram feitas para:
    - Melhorar a formatação e a estrutura do código, tornando-o mais legível.
    - Adicionar mensagens de erro personalizadas para os campos de senha e confirmação de senha.
    - Garantir que a validação de senhas coincida corretamente, especificando o caminho da mensagem de erro.

--- src/action/edit-user-action.ts
+++ src/action/edit-user-action.ts
@@ -1,104 +1,119 @@
-"use server"
+"use server";
 
 import { api } from "@/lib/axios";
 import { cookies } from "next/headers";
 import { NewUserSchema, NewUserSchemaType } from "@/schemas/new-user.schema";
-import { editProfileSchemaType, editProfileSchemaTransformed } from "@/schemas/edit-profile.schema"
+import {
+  editProfileSchemaType,
+  editProfileSchemaTransformed,
+} from "@/schemas/edit-profile.schema";
 
 export async function userProfileEditAction(
-    formData: editProfileSchemaType,
-): Promise<{ success: boolean, message: string }> {
+  formData: editProfileSchemaType
+): Promise<{ success: boolean; message: string; data?: any }> {
+  try {
+    const result = editProfileSchemaTransformed.safeParse(formData);
+    const newFormData = result.data!;
+    const token = cookies().get("token")?.value;
+    const parsedData: editProfileSchemaType = {
+      email: newFormData.email,
+      nome: newFormData.nome,
+      password: newFormData.password,
+      confirmPassword: newFormData.password,
+    };
 
-    try {
-        const result = editProfileSchemaTransformed.safeParse(formData);
-        const newFormData = result.data!
-        const token = cookies().get("token")?.value;
-        const parsedData: editProfileSchemaType = {
-            email: newFormData.email,
-            nome: newFormData.nome,
-            password: newFormData.password,
-            confirmPassword: newFormData.password
+    const response = await api
+      .patch(
+        `/users`,
+        {
+          name: parsedData.nome,
+          email: parsedData.email,
+          password: parsedData.password,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      )
+      .then((response) => response)
+      .catch((error) => error.response.data);
 
-        const response = await api.patch(`/users`, parsedData, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-            .then(response => response)
-            .catch(error => error.response.data)
-
-        if (response.status === 200) {
-            return {
-                success: true,
-                message: "Usuário editado com sucesso"
-            }
-        }
-
-        if (response.statusCode === 404) {
-            return {
-                success: false,
-                message: "Usuário não encontrado",
-            };
-        }
-
-        return {
-            success: false,
-            message: response.message
-        }
-    } catch (error) {
-        return {
-            success: false,
-            message: "Erro ao editar o usuário. Exceção: " + error
-        }
+    if (response.status === 200) {
+      return {
+        success: true,
+        message: "Usuário editado com sucesso",
+        data: response.data,
+      };
     }
+
+    if (response.statusCode === 404) {
+      return {
+        success: false,
+        message: "Usuário não encontrado",
+      };
+    }
+
+    return {
+      success: false,
+      message:
+        typeof response.message === "object"
+          ? response.message.join(", ")
+          : response.message,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "Erro ao editar o usuário. Exceção: " + error,
+    };
+  }
 }
 
 export async function adminEditUserAction(
-    formData: NewUserSchemaType,
-    cod_usuario: string
+  formData: NewUserSchemaType,
+  cod_usuario: string
 ): Promise<{ success: boolean; message: string }> {
-    try {
-        const result = NewUserSchema.safeParse(formData);
-        const newFormData = result.data!;
-        const token = cookies().get("token")?.value;
-        const parsedData: NewUserSchemaType = {
-            name: newFormData.name,
-            email: newFormData.email,
-            role: newFormData.role,
-        };
+  try {
+    const result = NewUserSchema.safeParse(formData);
+    const newFormData = result.data!;
+    const token = cookies().get("token")?.value;
+    const parsedData: NewUserSchemaType = {
+      name: newFormData.name,
+      email: newFormData.email,
+      role: newFormData.role,
+    };
 
-        const response = await api
-            .patch(`/users/${cod_usuario}`, parsedData, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            .then((response) => response)
-            .catch((error) => error.response.data);
+    const response = await api
+      .patch(`/users/${cod_usuario}`, parsedData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => response)
+      .catch((error) => error.response.data);
 
-        if (response.status === 200) {
-            return {
-                success: true,
-                message: "Usuário editado com sucesso",
-            };
-        }
-
-        if (response.statusCode === 404) {
-            return {
-                success: false,
-                message: "Usuário não encontrado",
-            };
-        }
-
-        return {
-            success: false,
-            message: response.message,
-        };
-    } catch (error) {
-        return {
-            success: false,
-            message: "Erro ao editar o usuário. Exceção: " + error,
-        };
+    if (response.status === 200) {
+      return {
+        success: true,
+        message: "Usuário editado com sucesso",
+      };
     }
+
+    if (response.statusCode === 404) {
+      return {
+        success: false,
+        message: "Usuário não encontrado",
+      };
+    }
+
+    return {
+      success: false,
+      message: response.message,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "Erro ao editar o usuário. Exceção: " + error,
+    };
+  }
 }
--- src/schemas/edit-profile.schema.ts
+++ src/schemas/edit-profile.schema.ts
@@ -1,27 +1,56 @@
-import z from "zod"
-import { maxEmailLength, maxNameLength, maxPasswordLength, minPasswordLength, passwordRegex } from "@/constants/auth"
+import z from "zod";
+import {
+  maxEmailLength,
+  maxNameLength,
+  maxPasswordLength,
+  minPasswordLength,
+  passwordRegex,
+} from "@/constants/auth";
 
-export const editProfileSchema = z.object({
+export const editProfileSchema = z
+  .object({
     email: z
-        .string().email("E-mail inválido")
-        .min(1, "O e-mail é obrigatório")
-        .max(maxEmailLength, `O e-mail deve ter no máximo ${maxEmailLength} caracteres`).optional(),
+      .string()
+      .email("E-mail inválido")
+      .min(1, "O e-mail é obrigatório")
+      .max(
+        maxEmailLength,
+        `O e-mail deve ter no máximo ${maxEmailLength} caracteres`
+      ),
     password: z
-        .string().regex(passwordRegex, "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial")
-        .min(minPasswordLength, `A senha deve ter no mínimo ${minPasswordLength} caracteres`)
-        .max(maxPasswordLength, `A senha deve ter no máximo ${maxPasswordLength} caracteres`).optional(),
-    confirmPassword: z
-        .string().regex(passwordRegex, "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial")
-        .min(minPasswordLength, `A senha deve ter no mínimo ${minPasswordLength} caracteres`)
-        .max(maxPasswordLength, `A senha deve ter no máximo ${maxPasswordLength} caracteres`).optional(),
+      .string({
+        message: "Senha obrigatória",
+      })
+      .regex(
+        passwordRegex,
+        "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial"
+      )
+      .min(
+        minPasswordLength,
+        `A senha deve ter no mínimo ${minPasswordLength} caracteres`
+      )
+      .max(
+        maxPasswordLength,
+        `A senha deve ter no máximo ${maxPasswordLength} caracteres`
+      ),
+    confirmPassword: z.string({
+      message: "Confirme sua senha",
+    }),
     nome: z
-        .string().min(1, "O nome é obrigatório")
-        .max(maxNameLength, `O nome deve ter no máximo ${maxNameLength} caracteres`).optional(),
-    })
-    .refine(data => data.password === data.confirmPassword, {
-        message: "As senhas não coincidem",
-    })
+      .string()
+      .min(1, "O nome é obrigatório")
+      .max(
+        maxNameLength,
+        `O nome deve ter no máximo ${maxNameLength} caracteres`
+      ),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  });
 
-export type editProfileSchemaType = z.infer<typeof editProfileSchema>
+export type editProfileSchemaType = z.infer<typeof editProfileSchema>;
 
-export const editProfileSchemaTransformed = editProfileSchema.transform(({ confirmPassword, ...data }) => data);
+export const editProfileSchemaTransformed = editProfileSchema.transform(
+  ({ confirmPassword, ...data }) => data
+);
