ecad7c3ddcd56fe819e4605c9e95b376cf2a5df1
Adler Castro <adlercastro01@gmail.com>
2024-09-28 12:35:53 -0300

    :recycle: refactor: Padronizando a função de submit

--- src/app/(with-auth)/consumer-units/components/alertAddConsumerUnit.tsx
+++ src/app/(with-auth)/consumer-units/components/alertAddConsumerUnit.tsx
@@ -35,15 +35,11 @@ import {
 export const AlertAddConsumerUnit = () => {
   const queryClient = useQueryClient();
   const { toast } = useToast();
-  const [value, setValue] = useState<string>("");
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false);
 
   const form = useForm<LinkConsumerUnitSchemaType>({
-    defaultValues: {
-      number: "",
-    },
     resolver: zodResolver(LinkConsumerUnitSchema),
   });
 
@@ -51,47 +47,37 @@ export const AlertAddConsumerUnit = () => {
     router.prefetch(Routes.ConsumerUnits);
     setLoading(true);
 
-    await linkConsumerUnitAction(values)
-      .then((response) => {
-        console.log(response);
-        if (response.success) {
-          toast({
-            title: "Sucesso",
-            description: response.message,
-            variant: "success",
-          });
-          queryClient.invalidateQueries({ queryKey: ["consumer-units"] });
-          router.push(Routes.ConsumerUnits);
-          setOpen(false);
-        } else {
-          toast({
-            title: "Erro",
-            description: response.message,
-            variant: "destructive",
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        toast({
-          title: "Erro",
-          description: error.message,
-          variant: "destructive",
-        });
-      })
-      .finally(() => {
-        setLoading(false);
+    const response = await linkConsumerUnitAction(values).finally(() => {
+      setLoading(false);
+    });
+
+    console.log(response);
+    if (response.success) {
+      toast({
+        title: "Sucesso",
+        description: response.message,
+        variant: "success",
       });
+      queryClient.invalidateQueries({ queryKey: ["consumer-units"] });
+      router.push(Routes.ConsumerUnits);
+      setOpen(false);
+    } else {
+      toast({
+        title: "Erro",
+        description: response.message,
+        variant: "destructive",
+      });
+    }
   }
 
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogTrigger>
-        <Button variant="solar" className="w-fit p-3 gap-2">
+      <Button variant="solar" className="w-fit p-3 gap-2" asChild>
+        <AlertDialogTrigger>
           <CirclePlus size={24} />
           Adicionar
-        </Button>
-      </AlertDialogTrigger>
+        </AlertDialogTrigger>
+      </Button>
       <AlertDialogContent className="max-w-72 sm:max-w-96 flex flex-col gap-8 items-center justify-center p-9 overflow-hidden border-none rounded-[1.25rem]">
         <AlertDialogCancel asChild>
           <Button className="self-end p-2 border-none">
@@ -100,43 +86,44 @@ export const AlertAddConsumerUnit = () => {
         </AlertDialogCancel>
 
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(linkConsumerUnit)}>
+          <form
+            onSubmit={form.handleSubmit(linkConsumerUnit)}
+            className="flex flex-col gap-8 items-center"
+          >
             <AlertDialogTitle className="text-3xl font-bold text-secondary-foreground text-center items-stretch">
               <span className="text-solaris-primary">Registrar</span> unidade
               consumidora
             </AlertDialogTitle>
-            <AlertDialogDescription className="flex flex-col gap-4 text-center">
-              <FormField
-                control={form.control}
-                name="number"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        label="Número da unidade consumidora"
-                        placeholder="0 0 0 0 0 0 0 0"
-                        className="text-center"
-                        required
-                        invalid={!!!form.formState.errors.number}
-                        disabled={loading}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </AlertDialogDescription>
+
+            <FormField
+              control={form.control}
+              name="number"
+              render={({ field }) => (
+                <FormItem className="max-w-36 flex flex-col gap-4 text-center items-center">
+                  <FormControl>
+                    <Input
+                      required
+                      {...field}
+                      label="Número da unidade consumidora"
+                      placeholder="0 0 0 0 0 0 0 0"
+                      className="text-center"
+                      invalid={!!!form.formState.errors.number}
+                      disabled={loading}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
             <Button
               type="submit"
               variant="solar"
-              disabled={value === ""}
               loading={loading}
+              disabled={loading}
+              className="max-w-36 w-full"
             >
-              <AlertDialogAction className="bg-transparent hover:bg-transparent">
-                Concluir
-              </AlertDialogAction>
+              Concluir
             </Button>
           </form>
         </Form>
