6add5e2dbdaa426d7cce1b12cd47a8a63d06fb7a
Marco Antônio <marcopierozan20@gmail.com>
2024-10-03 11:02:58 -0300

    :wrench: fix: small fixes

--- src/app/(with-auth)/users/[id]/page.tsx
+++ src/app/(with-auth)/users/[id]/page.tsx
@@ -69,6 +69,7 @@ export default function UserConsumerUnit() {
           },
         }
       );
+      console.log(response.data);
       return response.data;
     },
     placeholderData: keepPreviousData,
